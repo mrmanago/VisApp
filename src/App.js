66eaef29ea88b1cec2733d2115947c287010c242
Andini <a.a.f.andini.aulia.faramita@student.tue.nl>
2021-05-19 09:14:56 +0200

    Implement basic theme
    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import Header from './components/Header'
-import Nav from './components/Nav'
-import Footer from './components/Footer'
-import Button from "./components/Button"
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Visualize from './Visualize';
+import Info from './Info';
+import Footer from './components/Footer';
+import Button from "./components/Button";
 import VisWindow from "./components/VisWindow";
+import { Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-      <div>
-          <Header />
-            <div>
-                <Nav />
-                <div className="Content">
-                    <Button />
-                    <VisWindow />
+      <div className="App">
+              <Navbar />
+                <div className="Box">
+                  <Header />
+                  <Route exact path="/Visualize" components={Visualize} />
+                  <Route exact path="/Info" components={Info} />
+                  <Button />
+                  <VisWindow />
+                  <Footer />
                 </div>
-            </div>
-          <Footer />
       </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import './react-sidenav.css';
+
+const Navbar = () => {
+    return (
+        <SideNav
+    onSelect={(selected) => {
+        // Add your code here
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home">
+    <NavItem eventKey="charts">
+            <NavIcon>
+                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Charts
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="home">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
+    )
+}
+
+export default Navbar
