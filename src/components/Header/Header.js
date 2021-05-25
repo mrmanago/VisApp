40d986bedf6f58affd6e35a1f5450be5e6e3ee44
Andini <a.a.f.andini.aulia.faramita@student.tue.nl>
2021-05-26 01:00:42 +0200

    Wrote base code for components of webpage
    Base code for components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,11 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1>Title</h1>
+            <h1>VIZ IT</h1>
+            <h2>Visualize the beautiful mess of your data</h2>
+            <iframe title="Introduction Video" width="854" height="480"
+            src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
         </div>
     )
 }
