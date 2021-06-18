4717caeb9297be0efb163c7323434786ce75cf01
yangabiceps <yanislav.herold@gmail.com>
2021-06-18 17:59:43 +0300

    Added the summary node link visualization Made changes in the app css and js

---
+++ src/components/VisWindow/SummaryNode.js
@@ -0,0 +1,194 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+class SummaryNode extends Component {
+
+    componentDidMount(){
+
+        d3.json("/sample-datasets/node-link-value.json", function(error,data) {
+            if (error) throw error;
+
+            const width = 1024,
+                height = 640;
+
+            let beautifulVariable=true;
+
+            //Initializing chart
+            const svg = d3.select('.chart')
+                .attr('width', width)
+                .attr('height', height)
+                .append('g');
+
+            //Creating tooltip
+            const tooltip = d3.select('.Vis3')
+                .append('div')
+                .attr('class', 'tooltip')
+                .html('Tooltip');
+            const marker = d3.select("svg").append("svg:defs")
+                .append("svg:marker")
+                .attr("id", "triangle")
+                .attr("refX", 6)
+                .attr("refY", 6)
+                .attr("markerWidth", 30)
+                .attr("markerHeight", 30)
+                .attr("orient", "auto")
+                .append("path")
+                .attr("d", "M 0 0 12 6 0 12 3 6")
+                .style("fill", "red");
+
+            const scale = d3.scaleOrdinal(d3.schemeCategory10);
+
+
+            //Initializing force simulation
+            const simulation = d3.forceSimulation()
+                .force('link', d3.forceLink())
+                .force('charge', d3.forceManyBody().strength(-50))
+                .force('collide', d3.forceCollide().radius(20))
+                .force('center', d3.forceCenter(width / 2, height / 2))
+                .force("y", d3.forceY(0))
+                .force("x", d3.forceX(0));
+
+
+            //Drag functions
+            const dragStart = (event, d) => {
+                if (!event.active) simulation.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            };
+
+            const drag = (event, d) => {
+                d.fx = event.x;
+                d.fy = event.y;
+            };
+
+            const dragEnd = (event, d) => {
+                if (!event.active) simulation.alphaTarget(0);
+                d.fx = null;
+                d.fy = null;
+            }
+
+            const mouseOver = (event, d) => {
+                tooltip.html("id:"+d.id+"<br/> email:"+d.email+"<br/> Job Title:"+d.jobTitle)
+                    .transition()
+                    .delay(100)
+                    .duration(1000)
+                    .style('left', event.pageX + 5 +'px')
+                    .style('top', event.pageY + 5 + 'px')
+                    .style('opacity', .9);
+            }
+
+            const mouseClick = d => {
+                if(beautifulVariable) {
+                    node
+                        .filter((c) => {
+                            c.selected = true
+                        })
+
+                    link
+                        .filter((c) => {
+                            if (c.target.id == d.id) {
+
+                                c.source.selected = false;
+                                c.target.selected = false;
+                            }
+                            if (c.source.id == d.id) {
+
+                                c.source.selected = false;
+                                c.target.selected = false;
+                            }
+                            return c.target.id !== d.id && c.source.id !== d.id;
+                        })
+                        .style("visibility", "hidden")
+
+                    node
+                        .filter(c => c.selected)
+                        .attr("opacity", 0);
+                }
+                beautifulVariable=false;
+            }
+
+            const mouseOut = () => {
+                tooltip.style('opacity', 0)
+                    .style('left', '400px')
+                    .style('top', '400px');
+            }
+
+            const mouseClickButton =() => {
+                beautifulVariable=true;
+                node.attr("opacity", 1);
+                link.style("visibility","visible");
+            }
+
+            //Creating links
+            const link = svg
+                .selectAll('line')
+                .data(data.links).enter()
+                .append('line')
+                .style("stroke", "#aaa")
+                .attr('stroke-width', (d)=>d.weight/100);
+
+            //Creating nodes
+            const node = svg
+                .selectAll('circle')
+                .data(data.nodes).enter()
+                .append('circle')
+                .attr("r", 7)
+                .style("fill", (d) => scale(d.jobTitle))
+                .on('mouseover', mouseOver)
+                .on('mouseout', mouseOut)
+                .on('click', mouseClick)
+                .call(d3.drag()
+                    .on('start', dragStart)
+                    .on('drag', drag)
+                    .on('end', dragEnd)
+                );
+
+            //Setting location when ticked
+            const ticked = () => {
+                link
+                    .attr("x1", d => { return d.source.x; })
+                    .attr("y1", d => { return d.source.y; })
+                    .attr("x2", d => { return d.target.x; })
+                    .attr("y2", d => { return d.target.y; });
+
+                node
+                    .attr("cx", function (d) { return d.x; })
+                    .attr("cy", function(d) { return d.y; });
+            };
+
+            //Starting simulation
+            simulation.nodes(data.nodes)
+                .on('tick', ticked);
+
+            simulation.force('link')
+                .links(data.links);
+
+            const rect = svg.append('rect','text')
+                .attr("x", 0)
+                .attr("y", 550)
+                .attr("width", 60)
+                .attr("height", 60)
+                .style("fill", "black")
+                .on('click', mouseClickButton);
+
+            const text = svg.append('text')
+                .attr('x', 5)
+                .attr('y', 585)
+                .attr('stroke', 'white')
+                .style("font-size", 19)
+                .text("Reset")
+                .on('click', mouseClickButton);
+        });
+    }
+
+    render(){
+        return (
+            <div className='Vis3'>
+                    <svg className='chart'>
+                    </svg>
+            </div>
+        );
+    }
+
+}
+export default SummaryNode
---
+++ src/components/VisWindow/VisWindow2.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as d3 from "d3";
+import SummaryNode from "./SummaryNode";
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const VisWindow2 = () => {
+    return (
+        <div className="VisWindow2">
+            <SummaryNode/>
+        </div>
+    )
+}
+
+export default VisWindow2
